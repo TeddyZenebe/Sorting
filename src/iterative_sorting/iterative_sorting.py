df3c6cdef6ee3171fc7e06a4914c5d2f88e946a9
Teddy <tfikrie@yahoo.com>
2020-03-03 22:09:27 -0500

    merge sorting completed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,12 +6,14 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
-
+        for j in range(i+1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
 
         # TO-DO: swap
-
+        arrlist = arr[i]
+        arr[i] = arr[smallest_index]
+        arr[smallest_index ] = arrlist
 
 
 
@@ -20,6 +22,13 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    for j in range(len(arr)-1,0,-1):
+        for i in range(j):
+            if arr[i] > arr[i+1]:
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+
 
     return arr
 
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,6 +3,21 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
+    arr_a_index = 0
+    arr_b_index = 0
+    for i in range(0, len(merged_arr)):
+        if arr_a_index == len(arrA):
+            merged_arr[i] = arrB[arr_b_index]
+            arr_b_index +=1 
+        elif arr_b_index == len(arrB):
+            merged_arr[i] = arrA[arr_a_index]
+            arr_a_index += 1 
+        elif arrA[arr_a_index] < arrB[arr_b_index]:
+            merged_arr[i] = arrA[arr_a_index]
+            arr_a_index += 1        
+        elif arrA[arr_a_index] > arrB[arr_b_index]:
+            merged_arr[i] = arrB[arr_b_index]
+            arr_b_index += 1     
     
     return merged_arr
 
@@ -10,6 +25,44 @@ def merge( arrA, arrB ):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        # Recursive call on each half
+        merge_sort(left)
+        merge_sort(right)
+
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+        
+        # Iterator for the main list
+        k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                # The value from the left half has been used
+                arr[k] = left[i]
+                # Move the iterator forward
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k]=right[j]
+            j += 1
+            k += 1
 
     return arr
 
